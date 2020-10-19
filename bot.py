1d9033262f4f881c8c091e4225304962e1df4e4b
Kardash <kardash.by@gmail.com>
2020-10-20 00:40:58 +0300

    added chrome

--- bot.py
+++ bot.py
@@ -38,13 +38,11 @@ def sticker_id(message):
 
 
 def send_screenshot(message):
-    bot.send_message(message.chat.id, "1")
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    bot.send_message(message.chat.id, "2")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get(url)
     bot.send_photo(message.chat.id, driver.get_screenshot_as_png())
