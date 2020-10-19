384a8aceedd494a467c3dc3ce7c28d488bf7a5c6
Kardash <kardash.by@gmail.com>
2020-10-20 00:34:51 +0300

    added chrome

--- bot.py
+++ bot.py
@@ -12,18 +12,7 @@ keyboard1.row('Привет', 'Пока')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    from selenium import webdriver
-    import os
-    bot.send_message(message.chat.id, "1")
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    bot.send_message(message.chat.id, "2")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    driver.get("https://google.com")
-    bot.send_message(message.chat.id, driver.get_screenshot_as_png())
+    send_screenshot(message)
 
 
 @bot.message_handler(content_types=['text'])
@@ -35,8 +24,7 @@ def send_text(message):
             if "Appointment dates are not available." in str(r.content):
                 time.sleep(60)
             else:
-                bot.send_message(message.chat.id,
-                                 'Доступны аппоинтменты: https://algeria.blsspainvisa.com/english/book_appointment.php')
+                send_screenshot(message)
     elif message.text.lower() == 'пока':
         bot.send_message(message.chat.id, 'Прощай, создатель')
     elif message.text.lower() == 'я тебя люблю':
@@ -46,6 +34,21 @@ def send_text(message):
 @bot.message_handler(content_types=['sticker'])
 def sticker_id(message):
     print(message)
+
+
+def send_screenshot(message):
+    from selenium import webdriver
+    import os
+    bot.send_message(message.chat.id, "1")
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    bot.send_message(message.chat.id, "2")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver.get("https://google.com")
+    bot.send_photo(message.chat.id, driver.get_screenshot_as_png())
 
 
 bot.polling(none_stop=True)
