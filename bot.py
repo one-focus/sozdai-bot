8f1d17d7115e15efa5adfa8de98ede6fb825e2e0
Kardash <kardash.by@gmail.com>
2020-10-26 22:03:48 +0300

    added trains

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import subprocess
+from lxml import html
 import time
 
 import requests
@@ -30,6 +30,32 @@ def send_text(message):
                 send_screenshot(message)
     elif message.text.lower() == 'пока':
         bot.send_message(message.chat.id, 'Прощай, создатель')
+    elif message.text.lower() == 'минск':
+        ra = requests.get(
+            "https://pass.rw.by/ru/route/?from=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA-%D0%A1%D0%B5%D0%B2%D0%B5%D1%80%D0%BD%D1%8B%D0%B9&from_exp=2100450&from_esr=140102&to=%D0%A0%D0%B0%D1%82%D0%BE%D0%BC%D0%BA%D0%B0&to_exp=&to_esr=&front_date=%D1%81%D0%B5%D0%B3%D0%BE%D0%B4%D0%BD%D1%8F&date=today")
+        ra_html = html.fromstring(ra.content)
+        types = ra_html.xpath(
+            '//div[@class="sch-table__body js-sort-body"]//div[@class="sch-table__train-type"]/span[@class="sch-table__route-type"]/text()')
+        departures = ra_html.xpath(
+            '//div[@class="sch-table__body js-sort-body"]//div[@class="sch-table__time train-from-time"]/text()')
+        result = ""
+        lenght = len(departures) if len(departures) < 3 else 3
+        for i in range(lenght):
+            result += f'{departures[i]} {types[i][:3]}\n'
+        bot.send_message(message.chat.id, result)
+    elif message.text.lower() == 'ратомка':
+        ra = requests.get(
+            "https://pass.rw.by/ru/route/?from=%D0%A0%D0%B0%D1%82%D0%BE%D0%BC%D0%BA%D0%B0&from_exp=&from_esr=&to=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA-%D0%A1%D0%B5%D0%B2%D0%B5%D1%80%D0%BD%D1%8B%D0%B9&to_exp=2100450&to_esr=140102&front_date=%D1%81%D0%B5%D0%B3%D0%BE%D0%B4%D0%BD%D1%8F&date=today")
+        ra_html = html.fromstring(ra.content)
+        types = ra_html.xpath(
+            '//div[@class="sch-table__body js-sort-body"]//div[@class="sch-table__train-type"]/span[@class="sch-table__route-type"]/text()')
+        departures = ra_html.xpath(
+            '//div[@class="sch-table__body js-sort-body"]//div[@class="sch-table__time train-from-time"]/text()')
+        result = ""
+        lenght = len(departures) if len(departures) < 3 else 3
+        for i in range(lenght):
+            result += f'{departures[i]} {types[i][:3]}\n'
+        bot.send_message(message.chat.id, result)
     elif message.text.lower() == 'я тебя люблю':
         bot.send_sticker(message.chat.id, 'CAADAgADZgkAAnlc4gmfCor5YbYYRAI')
 
@@ -52,7 +78,7 @@ def send_screenshot(message):
         driver.find_element_by_class_name('popup-appCloseIcon').click()
     except Exception:
         pass
-    bot.send_photo(message.chat.id, driver.get_screenshot_as_png())
+    bot.send_photo(message.chat.id, driver.get_screenshot_as_png(), reply_markup=InlineKeyboardButton("Option 1", callback_data='1'))
     bot.send_message(message.chat.id, "https://algeria.blsspainvisa.com/english/book_appointment.php")
 
 
