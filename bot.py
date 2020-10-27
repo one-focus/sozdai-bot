00843edde8cf482f8296efc95e67b086b4875268
Kardash <kardash.by@gmail.com>
2020-10-27 19:59:33 +0300

    added sleep

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ bot = telebot.TeleBot("1275523107:AAF_5t_r80J55Pl-JcVeLcVVOsl7kadqAc4")
 
 @bot.message_handler(commands=["start"])
 def inline(message):
+    bot.send_message(message.chat.id, "Чего угодно?")
     bot.send_message(message.chat.id, "Чего угодно?", reply_markup=main_menu_buttons())
 
 
