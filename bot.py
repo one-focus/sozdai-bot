2056c19384768840f591ad7f8a306708352a9ed3
Kardash <kardash.by@gmail.com>
2020-10-27 10:21:42 +0300

    added logging

--- bot.py
+++ bot.py
@@ -32,9 +32,24 @@ def send_text(message):
         bot.send_message(message.chat.id, 'Прощай, создатель')
     elif message.text.lower() == 'минск':
         bot.send_message(message.chat.id, 'минск')
-        message = get_trains(
-            "https://pass.rw.by/ru/route/?from=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA-%D0%A1%D0%B5%D0%B2%D0%B5%D1%80%D0%BD%D1%8B%D0%B9&from_exp=2100450&from_esr=140102&to=%D0%A0%D0%B0%D1%82%D0%BE%D0%BC%D0%BA%D0%B0&to_exp=&to_esr=&front_date=%D1%81%D0%B5%D0%B3%D0%BE%D0%B4%D0%BD%D1%8F&date=today")
-        bot.send_message(message.chat.id, message)
+        ra = requests.get("https://pass.rw.by/ru/route/?from=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA-%D0%A1%D0%B5%D0%B2%D0%B5%D1%80%D0%BD%D1%8B%D0%B9&from_exp=2100450&from_esr=140102&to=%D0%A0%D0%B0%D1%82%D0%BE%D0%BC%D0%BA%D0%B0&to_exp=&to_esr=&front_date=%D1%81%D0%B5%D0%B3%D0%BE%D0%B4%D0%BD%D1%8F&date=today")
+        ra_html = html.fromstring(ra.content)
+        print(f'ra_html:{ra_html}')
+        title = ra_html.xpath('//div[@class="sch-title__title h2"]/text()')
+        print(f'title:{title}')
+        date = ra_html.xpath('//div[@class="sch-title__date h3"]/text()')
+        print(f'date:{date}')
+        types = ra_html.xpath(
+            '//div[@class="sch-table__body js-sort-body"]//div[@class="sch-table__train-type"]/span[@class="sch-table__route-type"]/text()')
+        departures = ra_html.xpath(
+            '//div[@class="sch-table__body js-sort-body"]//div[@class="sch-table__time train-from-time"]/text()')
+        print(f'departures:{departures}')
+        lenght = len(departures) if len(departures) < 3 else 3
+        result = ''
+        for i in range(lenght):
+            result += f'{departures[i]} {types[i][:3]}\n'
+
+        bot.send_message(message.chat.id, result)
     elif message.text.lower() == 'ратомка':
         bot.send_message(message.chat.id, 'ратомка')
         message = get_trains(
